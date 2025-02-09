863d6145afd62e0e0d3ca049eb26f9e9d5b6bcbc
Smit Vekaria <smitvekaria56@gmail.com>
2025-02-09 13:45:18 -0500

    updated port binding

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT || 5500, async () => {
+app.listen(PORT, '0.0.0.0', async () => {
 	console.log(
-		`Subscription Tracker API is running on http://localhost:${PORT || 5500}`
+		`Subscription Tracker API is running on http://localhost:${PORT}`
 	);
 
 	await connectToDatabase();
